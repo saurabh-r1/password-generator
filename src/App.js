5b1a955006ba03868daf7e346d87adfc8837d341
saurabh-r1 <sidlock2@gmail.com>
2023-09-10 20:54:27 +0530

    generating password

--- src/App.js
+++ src/App.js
@@ -1,9 +1,98 @@
-
+import { Fragment, useCallback, useEffect, useRef, useState } from "react";
 
 
 function App() {
-  return(
-    <h1>Password generator</h1>
-  )
-}
+  const [length, setLength]=useState(8);
+  const [numberAllowed, setNumberAllowed]=useState(false);
+  const [charAllowed, setCharAllowed]=useState(false);
+  const [password, setPassword]=useState("");
+
+  const passwordRef = useRef(null);
+
+  const passwordGenerator=useCallback(()=>{
+    let pass=""
+    let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if(numberAllowed) str += "0123456789";
+    if(charAllowed) str += "!@#$%^&*()_-+=<>?/{}[]" ;
+
+    for(let i=1; i<=length;i++){
+      let char =Math.floor(Math.random()*str.length+1);
+      pass += str.charAt(char);
+    }
+    setPassword(pass);
+
+  },[length, numberAllowed, charAllowed,setPassword])
+
+  const copyPasswordToClipboard = useCallback(()=>{
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0,100);
+    window.navigator.clipboard.writeText(password);
+  },[password])
+
+  useEffect(()=>{
+    passwordGenerator();
+  },[length, numberAllowed, charAllowed, passwordGenerator])
+  return (
+    <Fragment>
+      <div className="w-full max-w-md mx-auto shadow-md
+      rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-800"
+      >
+        <h1 className="text-white text-center my-3">Password generator</h1>
+        <div className="flex shadow rounded-lg
+        overflow-hidden mb-4">
+          <input 
+          className="outline-none w-full py-1  px-3 "
+          type="text"
+          value={password}
+          placeholder="password"
+          readOnly
+          ref={passwordRef}
+           />
+           <button
+           onClick={copyPasswordToClipboard} 
+           className="outline-none bg-blue-700 text-white
+           px-3 py-0.5 shrink-0 "
+           >copy</button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input type="range"
+            min={6}
+            max={100}
+            value={length}
+            className="cursor-pointer"
+            onChange={(e)=>{
+              setLength(e.target.value)
+            }}
+             />
+             <label htmlFor="">Length:{length}</label>
+          </div>
+          <div>
+            <input 
+            type="checkbox"
+            defaultChecked={numberAllowed}
+            id="numberInput"
+            onChange={()=>{
+              setNumberAllowed((prev)=> !prev)
+            }}
+            />
+            <label htmlFor="numberInput">Number</label>
+          </div>
+          <div>
+            <input 
+            type="checkbox"
+            defaultChecked={charAllowed}
+            id="charInput"
+            onChange={()=>{
+              setCharAllowed((prev)=> !prev)
+            }}
+            />
+            <label htmlFor="charInput">Special Characters</label>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
 export default App;
